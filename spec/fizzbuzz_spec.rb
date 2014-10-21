d95251e5ab30b55387a2b5db3f4cad53f8dd3a0f
Scully87 <scully87.ts@googlemail.com>
2014-10-21 09:40:45 +0100

    refactored

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,35 +2,39 @@ require 'fizzbuzz'
 
 describe 'Fizzbuzz' do
 
-	context "knows that a number is divisible by" do
+	context "Knows that a number is divisible by" do
 
-		it '3' do
+		it 'Three' do
 			expect(is_divisible_by_three?(3)).to eq(true)
 		end
-		it '5' do
+
+		it 'Five' do
 			expect(is_divisible_by_five?(5)).to eq(true)
 		end
-		it '15' do
+
+		it 'Fifteen' do
 			expect(is_divisible_by_fifteen?(15)).to eq(true)
 		end
 
 	end
 
-	context "knows that a number is not divisible by" do
+	context "Knows that a number is not divisible by" do
 
-		it '3' do
+		it 'Three' do
 			expect(is_divisible_by_three?(1)).to eq(false)
 		end
-		it '5' do
+
+		it 'Five' do
 			expect(is_divisible_by_five?(1)).to eq(false)
 		end
-		it '15' do
+
+		it 'Fifteen' do
 			expect(is_divisible_by_fifteen?(1)).to eq(false)
 		end
 
 	end
 
-	context "while playing says" do
+	context "Whilst playing says" do
 
 		it '"Fizz", if a number is divisible by Three' do
 			expect(fizzbuzz(3)).to eq "Fizz"
