b08c2122ed932954c9f881425f7549862aa8f652
Scully87 <scully87.ts@googlemail.com>
2014-11-15 21:36:51 +0000

    updated read me

--- README.md
+++ README.md
@@ -1,10 +1,44 @@
-Creating a FizzBuzz challenge in Ruby
-=============================================
+Fizzbuzz
+==================
 
-Players take turns to count incrementally, 
-replacing any number that is
-divisible by three with the word 'Fizz' and any 
-number divisible by 
-five with the word 'Buzz'.
-Numbers that are divisible by three and five 
-will return 'Fizzbuzz'.
+###Creating a simple TDD Fizzbuzz Kata in Ruby
+
+####Technologies Used:
+
+  - Ruby
+  - Rspec
+
+#####To do list:
+
+  - [x] A number is/not divisible by three and says "Fizz"
+  - [x] A number is/not divisible by five and says "Buzz"
+  - [x] A number is/not divisible by fifteen and says "FizzBuzz"
+
+How to use
+----------
+Clone the repository:
+```shell
+$ git clone git@github.com:scully87/fizzbuzz.git
+```
+
+Change into the directory:
+```shell
+$ cd fizzbuzz
+```
+
+To run tests:
+```shell
+$ rspec
+```
+
+Play in irb:
+```shell
+$ irb
+ > require './lib/fizzbuzz.rb'
+```
+--------------------------------------------------
+
+Fizzbuzz created in other languages:
+
+  - [Fizzbuzz Javascript](https://www.github.com/scully87/Fizzbuzz_javascript)
+  - [Fizzbuzz IO](https://www.github.com/Scully87/fizzbuzz_io)
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,54 +2,183 @@ require 'fizzbuzz'
 
 describe 'Fizzbuzz' do
 
-	context "Knows that a number is divisible by" do
+	context 'Knows when a number is divisible by' do
 
 		it 'Three' do
-			expect(is_divisible_by_three?(3)).to eq(true)
+			expect(is_divisible_by_three(3)).to eq(true)
 		end
 
 		it 'Five' do
-			expect(is_divisible_by_five?(5)).to eq(true)
+			expect(is_divisible_by_five(5)).to eq(true)
 		end
 
 		it 'Fifteen' do
-			expect(is_divisible_by_fifteen?(15)).to eq(true)
+			expect(is_divisible_by_fifteen(15)).to eq(true)
 		end
 
 	end
 
-	context "Knows that a number is not divisible by" do
+	context 'Knows when a number is NOT divisible by' do
 
 		it 'Three' do
-			expect(is_divisible_by_three?(1)).to eq(false)
+			expect(is_divisible_by_three(1)).to eq(false)
 		end
 
 		it 'Five' do
-			expect(is_divisible_by_five?(1)).to eq(false)
+			expect(is_divisible_by_five(1)).to eq(false)
 		end
 
 		it 'Fifteen' do
-			expect(is_divisible_by_fifteen?(1)).to eq(false)
+			expect(is_divisible_by_fifteen(1)).to eq(false)
 		end
 
 	end
 
-	context "Whilst playing says" do
+	context 'Whilst playing says' do
 
-		it '"Fizz", if a number is divisible by Three' do
+		it '"Fizz" when a number is Divisible by Three' do
 			expect(fizzbuzz(3)).to eq "Fizz"
-		end 
-		it '"Buzz", if a number is divisible by Five' do
+		end
+
+		it '"Buzz" when a number is Divisible by Five' do
 			expect(fizzbuzz(5)).to eq "Buzz"
 		end
-		it '"FizzBuzz", if a number is divisible by Fifteen' do
-			expect(fizzbuzz(15)).to eq "FizzBuzz"
+
+		it '"Fizzbuzz" when a number is Divisible by Fifteen' do
+			expect(fizzbuzz(15)).to eq "Fizzbuzz"
 		end
 
-		it'A number not divisible by three or five' do
-			expect(fizzbuzz(1)).to eq(1)
+		it 'Returns a number that is NOT Divisible by Three or Five' do
+			expect(fizzbuzz(1)).to eq 1
 		end
-	
 	end
 
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# require 'fizzbuzz'
+
+# describe 'Fizzbuzz' do
+
+# 	context "Knows that a number is divisible by" do
+
+# 		it 'Three' do
+# 			expect(is_divisible_by_three?(3)).to eq(true)
+# 		end
+
+# 		it 'Five' do
+# 			expect(is_divisible_by_five?(5)).to eq(true)
+# 		end
+
+# 		it 'Fifteen' do
+# 			expect(is_divisible_by_fifteen?(15)).to eq(true)
+# 		end
+
+# 	end
+
+# 	context "Knows that a number is not divisible by" do
+
+# 		it 'Three' do
+# 			expect(is_divisible_by_three?(1)).to eq(false)
+# 		end
+
+# 		it 'Five' do
+# 			expect(is_divisible_by_five?(1)).to eq(false)
+# 		end
+
+# 		it 'Fifteen' do
+# 			expect(is_divisible_by_fifteen?(1)).to eq(false)
+# 		end
+
+# 	end
+
+# 	context "Whilst playing says" do
+
+# 		it '"Fizz", if a number is divisible by Three' do
+# 			expect(fizzbuzz(3)).to eq "Fizz"
+# 		end 
+# 		it '"Buzz", if a number is divisible by Five' do
+# 			expect(fizzbuzz(5)).to eq "Buzz"
+# 		end
+# 		it '"FizzBuzz", if a number is divisible by Fifteen' do
+# 			expect(fizzbuzz(15)).to eq "FizzBuzz"
+# 		end
+
+# 		it'A number not divisible by three or five' do
+# 			expect(fizzbuzz(1)).to eq(1)
+# 		end
+	
+# 	end
+
+# end
